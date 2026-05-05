3b6d464a3069c2b6dbd3503b037aa44531294965
Toorum-SSG <alan13022011@gmail.com>
2026-05-05 21:37:12 +0200

    Adding MainWindow class for cleaner code

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("PixelPaint");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800,600);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+            new MainWindow().setVisible(true);
         });
     }
 }
---
+++ src/MainWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame{
+
+    public MainWindow() {
+        setTitle("PixelPaint");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1100, 750);
+        setLocationRelativeTo(null);
+    }
+}
