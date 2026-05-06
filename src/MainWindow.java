5fb60f7183cb481de36bda981a471ffa1024bafb
Toorum-SSG <alan13022011@gmail.com>
2026-05-06 20:11:20 +0200

    Put canvas onto MainWindow

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame{
+    private CanvasPanel canvas;
 
     public MainWindow() {
         setTitle("PixelPaint");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1100, 750);
         setLocationRelativeTo(null);
+        canvas = new CanvasPanel(800,600);
+        JScrollPane scrollPane = new JScrollPane(canvas);
+        scrollPane.getViewport().setBackground(new Color(180, 180, 180));
+        add(scrollPane, BorderLayout.CENTER);
     }
+
 }
