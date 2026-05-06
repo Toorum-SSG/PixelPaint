af0af070acd960d07164f85177f659dac54ef667
Toorum-SSG <alan13022011@gmail.com>
2026-05-06 20:40:53 +0200

    Brush to draw

--- src/CanvasPanel.java
+++ src/CanvasPanel.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 public class CanvasPanel extends JPanel{
@@ -12,6 +14,30 @@ public class CanvasPanel extends JPanel{
         g2d = image.createGraphics();
         g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, width, height);
+
+        int[]lastMousePos = {0,0};
+        MouseAdapter mouseAdapter = new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e){
+                lastMousePos[0] = e.getX();
+                lastMousePos[1] = e.getY();
+                g2d.setColor(Color.BLACK);
+                g2d.fillOval(e.getX() - 5, e.getY() - 5, 10, 10);
+                repaint();
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                g2d.setColor(Color.BLACK);
+                g2d.setStroke(new BasicStroke(10, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                g2d.drawLine(lastMousePos[0], lastMousePos[1], e.getX(), e.getY());
+                lastMousePos[0] = e.getX();
+                lastMousePos[1] = e.getY();
+                repaint();
+            }
+        };
+        addMouseListener(mouseAdapter);
+        addMouseMotionListener(mouseAdapter);
     }
 
     @Override
@@ -19,4 +45,6 @@ public class CanvasPanel extends JPanel{
         super.paintComponent(graphics);
         graphics.drawImage(image, 0, 0, null);
     }
+
+
 }
