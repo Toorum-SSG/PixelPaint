9e3676d7e7d24e244e10c2e8541e4dc27c1f16bf
Toorum-SSG <alan13022011@gmail.com>
2026-05-07 20:23:56 +0200

    tools

--- src/CanvasPanel.java
+++ src/CanvasPanel.java
@@ -6,7 +6,37 @@ import java.awt.image.BufferedImage;
 
 public class CanvasPanel extends JPanel{
     private BufferedImage image;
+    private BufferedImage scratch;
     private Graphics2D g2d;
+    private Tool currentTool = Tool.BRUSH;
+    private Color primaryColor = Color.BLACK;
+    private Color secondaryColor = Color.WHITE;
+    private int brushSize = 10;
+    private float opacity = 1.0f;
+    private int startX;
+    private int startY;
+
+    public void setCurrentTool(Tool t) {
+        currentTool = t;
+    }
+
+    public void setPrimaryColor(Color c) {
+        primaryColor = c;
+    }
+
+    public void setBrushSize(int s) {
+        brushSize = s;
+    }
+
+    public void setOpacity(float o) {
+        opacity = o;
+    }
+
+    private void applySettings(){
+        g2d.setColor(primaryColor);
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
+        g2d.setStroke(new BasicStroke(brushSize, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+    }
 
     public CanvasPanel(int width, int height){
         setPreferredSize(new Dimension(width, height));
@@ -24,6 +54,8 @@ public class CanvasPanel extends JPanel{
                 g2d.setColor(Color.BLACK);
                 g2d.fillOval(e.getX() - 5, e.getY() - 5, 10, 10);
                 repaint();
+                startX = e.getX();
+                startY = e.getY();
             }
 
             @Override
@@ -34,6 +66,13 @@ public class CanvasPanel extends JPanel{
                 lastMousePos[0] = e.getX();
                 lastMousePos[1] = e.getY();
                 repaint();
+                switch (currentTool) {
+                    case LINE:
+                    case RECTANGLE:
+                    case CIRCLE:
+                        drawShape(startX, startY, e.getX(), e.getY());
+                        break;
+                }
             }
         };
         addMouseListener(mouseAdapter);
@@ -46,5 +85,21 @@ public class CanvasPanel extends JPanel{
         graphics.drawImage(image, 0, 0, null);
     }
 
-
+    private void drawShape(int x1, int y1, int x2, int y2) {
+        int x = Math.min(x1, x2);
+        int y = Math.min(y1, y2);
+        int w = Math.abs(x2 - x1);
+        int h = Math.abs(y2 - y1);
+        switch (currentTool) {
+            case LINE:
+                g2d.drawLine(x1,y1,x2,y2);
+                break;
+            case RECTANGLE:
+                g2d.drawRect(x,y,w,h);
+                break;
+            case CIRCLE:
+                g2d.drawOval(x,y,w,h);
+                break;
+        }
+    }
 }
---
+++ src/Tool.java
@@ -0,0 +1,3 @@
+public enum Tool {
+    BRUSH, ERASER, LINE, CIRCLE, RECTANGLE
+}
