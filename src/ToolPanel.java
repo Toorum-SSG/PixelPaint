5eb8418b181e1a4d7c0eb0b9949d9f16a0f7b795
Toorum-SSG <alan13022011@gmail.com>
2026-05-11 21:34:44 +0200

    ToolPanel #3

--- src/ToolPanel.java
+++ src/ToolPanel.java
@@ -8,6 +8,8 @@ public class ToolPanel extends JPanel {
         this.canvas = canvas;
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setPreferredSize(new Dimension(160, 0));
+        add(buildToolButtons());
+        add(buildSizeSlider());
     }
 
     private JButton toolBtn(String label, Tool tool) {
@@ -21,6 +23,19 @@ public class ToolPanel extends JPanel {
         p.add(toolBtn("Brush", Tool.BRUSH));
         p.add(toolBtn("Eraser", Tool.ERASER));
         p.add(toolBtn("Line", Tool.LINE));
+        return p;
+    }
+
+    private JPanel buildSizeSlider(){
+        JPanel p = new JPanel();
+        JLabel label = new JLabel("Size: 10");
+        JSlider slider = new JSlider(1, 60, 10);
+        slider.addChangeListener(e -> {
+            canvas.setBrushSize(slider.getValue());
+            label.setText("Size: " + slider.getValue());
+        });
+        p.add(label);
+        p.add(slider);
         return p;
     }
 }
